4656b26999420577849d4d5c02ba9093160a6816
anasryadi <anasryadi0@gmail.com>
2023-09-08 15:29:04 -0700

    added country and city option drop down meny

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,7 +1,47 @@
+"use client";
+
 import { Country, City } from "country-state-city";
+import { useState } from "react";
+import Select from "react-select";
+
+type option = {
+  value: {
+    latitude: string;
+    longitude: string;
+    isoCode: string;
+  };
+  label: string;
+} | null;
+
+type cityOption = {
+  value: {
+    latitude: string;
+    longitude: string;
+    countryCode: string;
+    name: string;
+    stateCode: string;
+  };
+  label: string;
+} | null;
+
+const options = Country.getAllCountries().map((country) => ({
+  value: {
+    latitude: country.latitude,
+    longitude: country.longitude,
+    usoCode: country.isoCode,
+  },
+  label: country.name,
+}));
 
 function CityPicker() {
-  return <div>CityPicker</div>;
+  const [selectedCountry, setSelectedCountry] = useState<option>(null);
+  const [selectedCity, setSelectedCity] = useState<option>(null);
+
+  return (
+    <div>
+      <Select options={options} />
+    </div>
+  );
 }
 
 export default CityPicker;
