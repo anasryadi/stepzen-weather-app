52d19de5ea354f4ffdd5cf6c16312db560b85017
anasryadi <anasryadi0@gmail.com>
2023-09-24 15:57:41 -0700

    Added weatherCodeToString to convert weather codes to text

--- components/InformationPanel.tsx
+++ components/InformationPanel.tsx
@@ -19,6 +19,46 @@ function InformationPanel({ city, lat, long, results }: Props) {
           Long/Lat: {long}, {lat}
         </p>
       </div>
+      <CityPicker />
+
+      <hr className="my-10" />
+
+      <div className="mt-5 flex items-center justify-between space-x-10 mb-5">
+        <div>
+            <p className="text-xl">
+                {new Date().toLocaleDateString("en-GB", {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                })}
+            </p>
+            <p className="font-extralight">
+                Timezone: {Intl.DateTimeFormat().resolvedOptions().timeZone}
+            </p>
+        </div>
+        <p className="text-xl font-bold uppercase">
+            {new Date().toLocaleTimeString("en-GB", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+            })}
+        </p>
+      </div>
+      <hr className="mt-10 mb-5" />
+
+      <div>
+        <div>
+            {/* image */}
+
+            <div>
+                <p>{results.current_weather.temperature.toFixed(1)}°C</p>
+                <p>
+                {/* weather code */}
+                </p>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
