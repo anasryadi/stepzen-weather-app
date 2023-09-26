24756677e389ca0716cf73e9763e5cdf776ab1dd
anasryadi <anasryadi0@gmail.com>
2023-09-25 18:35:36 -0700

    added the rain chart

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,6 +1,7 @@
 import { getClient } from "@/apollo-client";
 import CalloutCard from "@/components/CalloutCard";
 import InformationPanel from "@/components/InformationPanel";
+import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
 import TempChart from "@/components/TempChart";
 import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
@@ -93,7 +94,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
         <div className="space-y-3">
           <TempChart results={results} />
-          {/* RainChart */}
+          <RainChart results={results} />
           {/* HumidityChart */}
         </div>
       </div>
