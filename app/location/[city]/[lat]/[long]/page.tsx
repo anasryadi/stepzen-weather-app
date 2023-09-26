ea4f462bdde445a95dfb99ac774eac07c4c1130f
anasryadi <anasryadi0@gmail.com>
2023-09-25 18:58:49 -0700

    added the humidity chart and the loading button

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,5 +1,6 @@
 import { getClient } from "@/apollo-client";
 import CalloutCard from "@/components/CalloutCard";
+import HumidityChart from "@/components/HumidityChart";
 import InformationPanel from "@/components/InformationPanel";
 import RainChart from "@/components/RainChart";
 import StatCard from "@/components/StatCard";
@@ -95,7 +96,7 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
         <div className="space-y-3">
           <TempChart results={results} />
           <RainChart results={results} />
-          {/* HumidityChart */}
+          <HumidityChart results={results} />
         </div>
       </div>
     </div>
